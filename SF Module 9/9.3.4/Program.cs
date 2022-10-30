78a228c68cf8183cff680b8d463a4950866c6f50
Dmitry <ryabchikk@rambler.ru>
2022-10-30 19:54:01 +0300

    9.3.8

--- SF Module 9/9.3.4/Program.cs
+++ SF Module 9/9.3.4/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        del del = Method1;
+        del += Method2;
+
+        del(5, 5);
+
+        del -= Method1;
+
+        del (6,6);
+    }
+    delegate void del(int a, int b);
+
+    public static void Method1(int A, int B)
+    {
+        int result = A - B;
+        Console.WriteLine(result);
+    }
+
+    public static void Method2(int A, int B)
+    {
+        int result = A + B;
+        Console.WriteLine(result);
+    }
+}
