304fa9f77a37ee7dd7840767c0747f802113077a
Dmitry <ryabchikk@rambler.ru>
2022-10-30 18:54:03 +0300

    9.3.4

--- SF Module 9/9.3.3/Program.cs
+++ SF Module 9/9.3.3/Program.cs
@@ -7,6 +7,9 @@
         DecDelegate DecDelegate = Dec;
         int result = DecDelegate.Invoke(6, 5);
         Console.WriteLine(result);
+
+        result = DecDelegate(6, 5);
+        Console.WriteLine(result);
     }
 
     public static int Dec(int A, int B)
