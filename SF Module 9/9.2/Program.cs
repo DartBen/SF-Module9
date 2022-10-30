6f0f97470c735dbc1d24228bde9b1eeaf4c031de
Dmitry <ryabchikk@rambler.ru>
2022-10-30 18:14:32 +0300

    9.2

---
+++ SF Module 9/9.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            throw new ArgumentOutOfRangeException("Ошибка");
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            WriteLine("Исключение :ArgumentOutOfRangeException");
+        }
+        finally
+        {
+
+        }
+
+        try
+        {
+            throw new RankException("");
+        }
+        catch (RankException ex)
+        {
+            WriteLine(ex.GetType());
+        }
+        finally
+        {
+        }
+
+
+
+    }
+}
+
---
+++ SF Module 9/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Exception exc = new Exception("Message");
+
+        exc.Data.Add("Дата создания:",DateTime.Now);
+
+        exc.HelpLink = "https://metanit.com/";
+
+
+    }
+}
