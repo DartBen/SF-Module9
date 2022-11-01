93a81915de9667099903c8d78fab4399876863b2
Dmitry <ryabchikk@rambler.ru>
2022-11-01 17:53:58 +0300

    9.4.4

---
+++ SF Module 9/9.4.3/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+
+    public delegate Car del();
+    public static Car CarHandler()
+    {
+        return null;
+    }
+
+    public static Lexus LexusHandler()
+    {
+        return null;
+    }
+
+    private static void Main(string[] args)
+    {
+        del del = LexusHandler;
+    }
+}
+
+class Car { }
+class Lexus : Car { }
---
+++ SF Module 9/9.4.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Linq;
+
+internal class Program
+{
+    public delegate void delegat(Child c);
+
+    public static void ParentInfo(Parent p)
+    {
+        Console.WriteLine(p.GetType());
+    }
+
+
+    private static void Main(string[] args)
+    {
+        delegat del = ParentInfo;
+
+        del.Invoke(new Child());
+
+    }
+}
+
+class Parent { }
+class Child : Parent { }
+
+
+
